203cd91cff8fd8694a459d78ed96c017de6c2b1b
Jannes96 <106305014+Jannes96@users.noreply.github.com>
2025-04-23 20:57:05 +0200

    .

--- homework_2/src/homework_2_task_2/Librarian.java
+++ homework_2/src/homework_2_task_2/Librarian.java
@@ -1,5 +1,15 @@
 package homework_2_task_2;
 
-public class Librarian {
+public class Librarian extends User{
 
+	String department = "Reference";
+	
+	@Override
+	public String getRole() {
+		return this.name + "Librarian";
+	}
+	
+	public void work() {
+		//do some work
+	}
 }
--- homework_2/src/homework_2_task_2/User.java
+++ homework_2/src/homework_2_task_2/User.java
@@ -2,4 +2,13 @@ package homework_2_task_2;
 
 public class User {
 
+	String name = "Unknown";
+	public String getRole() {
+		return "General user";
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
 }
